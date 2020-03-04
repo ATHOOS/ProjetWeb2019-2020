8cd30ed191708f0a7722fd5428e2d7460b6e7304
tanguybmx <tanguyxp@live.fr>
2020-03-04 11:18:54 +0100

    navigation entre menu
    voir problème lors du changement d'un menu qui se trouve dans la même page
    (conctact et Qu'est ce que c'est?)

--- SITE/assets/js/myjs.js
+++ SITE/assets/js/myjs.js
@@ -1,16 +1,74 @@
+//layout
+
+function removeClassActive(){
+    $('#navAccueil').removeClass('active');
+    $('#navWorkshops').removeClass('active');
+    $('#navBoiteId').removeClass('active');
+    $('#navProfil').removeClass('active');
+    $('#navAbout').removeClass('active');
+    $('#navContact').removeClass('active');
+    
+}
+
+function addClassActive(p){
+    p+='';
+    $('#'+p).addClass('active');
+}
+
 function connexion(){
     $('#content').load("assets/inc/login/connexion.html");
+
+    removeClassActive();
+
 
 }
 
 function creeCompte(){
     $('#content').load("assets/inc/login/inscription.html");
+
 }
 
 function dejaMembre(){
     $('#content').load("assets/inc/login/connexion.html");
+
 }
 
-function acceuil(){
-    $('#content').load("assets/inc/acceuil.html");
+function accueil(){
+    $('#content').load("assets/inc/accueil.html");
+
+    removeClassActive();
+    addClassActive('navAccueil');
+}
+
+function workshops(){
+    $('#content').load("assets/inc/workshops.html");
+
+    removeClassActive();
+    addClassActive('navWorkshops');
+}
+
+function boiteId(){
+    $('#content').load("assets/inc/boiteId.html");
+
+    removeClassActive();
+    addClassActive('navBoiteId');
+}
+
+function profil(){
+    $('#content').load("assets/inc/profil.html");
+
+    removeClassActive();
+    addClassActive('navProfil');
+}
+
+function about(){
+
+    removeClassActive();
+    addClassActive('navAbout');
+}
+
+function contact(){
+
+    removeClassActive();
+    addClassActive('navContact');
 }
