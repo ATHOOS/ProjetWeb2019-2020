f17ebb7fd46412d6cd4d18a1f648beb35c3b50ab
RemyVase <remy.vase3@hotmail.fr>
2020-04-04 14:34:16 +0200

    Design de boite à idées DONE

--- SITE/assets/inc/boiteId.php
+++ SITE/assets/inc/boiteId.php
@@ -1,18 +1,325 @@
 <?php
 session_start();
-$_SESSION['pageActuel']='boiteId.php';
+$_SESSION['pageActuel'] = 'boiteId.php';
 ?>
-<main id="content">
-
 
 <script>
     removeClassActive();
     addClassActive('navBoiteId');
 </script>
+
+<head>
+    <title>Outils d'administration</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/inc/admin/css/style.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap User Management Data Table</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        body {
+            color: #566787;
+            background: #f5f5f5;
+            font-family: 'Varela Round', sans-serif;
+            font-size: 13px;
+        }
+
+        .table-wrapper {
+            background: #fff;
+            padding: 20px 25px;
+            margin: 30px 0;
+            border-radius: 3px;
+            box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+        }
+
+        .table-title {
+            padding-bottom: 15px;
+            background: #eb5d1e;
+            color: #fff;
+            padding: 16px 30px;
+            margin: -20px -25px 10px;
+            border-radius: 3px 3px 0 0;
+        }
+
+        .table-title h2 {
+            margin: 5px 0 0;
+            font-size: 24px;
+        }
+
+        .table-title .btn {
+            color: #566787;
+            float: right;
+            font-size: 13px;
+            background: #fff;
+            border: none;
+            min-width: 50px;
+            border-radius: 2px;
+            border: none;
+            outline: none !important;
+            margin-left: 10px;
+        }
+
+        .table-title .btn:hover,
+        .table-title .btn:focus {
+            color: #566787;
+            background: #eb5d1e;
+        }
+
+        .table-title .btn i {
+            float: left;
+            font-size: 21px;
+            margin-right: 5px;
+        }
+
+        .table-title .btn span {
+            float: left;
+            margin-top: 2px;
+        }
+
+        table.table tr th,
+        table.table tr td {
+            border-color: #e9e9e9;
+            padding: 12px 15px;
+            vertical-align: middle;
+        }
+
+        table.table tr th:first-child {
+            width: 60px;
+        }
+
+        table.table tr th:last-child {
+            width: 100px;
+        }
+
+        table.table-striped tbody tr:nth-of-type(odd) {
+            background-color: #fcfcfc;
+        }
+
+        table.table-striped.table-hover tbody tr:hover {
+            background: #f5f5f5;
+        }
+
+        table.table th i {
+            font-size: 13px;
+            margin: 0 5px;
+            cursor: pointer;
+        }
+
+        table.table td:last-child i {
+            opacity: 0.9;
+            font-size: 22px;
+            margin: 0 5px;
+        }
+
+        table.table td a {
+            font-weight: bold;
+            color: #566787;
+            display: inline-block;
+            text-decoration: none;
+        }
+
+        table.table td a:hover {
+            color: #eb5d1e;
+        }
+
+        table.table td a.settings {
+            color: #eb5d1e;
+        }
+
+        table.table td a.delete {
+            color: #eb5d1e;
+        }
+
+        table.table td i {
+            font-size: 19px;
+        }
+
+        table.table .avatar {
+            border-radius: 50%;
+            vertical-align: middle;
+            margin-right: 10px;
+        }
+
+        .status {
+            font-size: 30px;
+            margin: 2px 2px 0 0;
+            display: inline-block;
+            vertical-align: middle;
+            line-height: 10px;
+        }
+
+        .text-success {
+            color: #eb5d1e;
+        }
+
+        .text-info {
+            color: #eb5d1e;
+        }
+
+        .text-warning {
+            color: #eb5d1e;
+        }
+
+        .text-danger {
+            color: #eb5d1e;
+        }
+
+        .pagination {
+            float: right;
+            margin: 0 0 5px;
+        }
+
+        .pagination li a {
+            border: none;
+            font-size: 13px;
+            min-width: 30px;
+            min-height: 30px;
+            color: #999;
+            margin: 0 2px;
+            line-height: 30px;
+            border-radius: 2px !important;
+            text-align: center;
+            padding: 0 6px;
+        }
+
+        .pagination li a:hover {
+            color: #666;
+        }
+
+        .pagination li.active a,
+        .pagination li.active a.page-link {
+            background: #eb5d1e;
+        }
+
+        .pagination li.active a:hover {
+            background: #eb5d1e;
+        }
+
+        .pagination li.disabled i {
+            color: #ccc;
+        }
+
+        .pagination li i {
+            font-size: 16px;
+            padding-top: 6px
+        }
+
+        .hint-text {
+            float: left;
+            margin-top: 10px;
+            font-size: 13px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+<main id="content" style="margin-top:6em">
+
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <h2>Boite à idées <b>Professeurs</b></h2>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Titre</th>
+                        <th>Sujet</th>
+                        <th>Voter oui</th>
+                        <th>Voter non</th>
+                        <th>% avis</th>
+                    </tr>
+                </thead>
+                <tbody id="listeIdeesProfs">
+
+                </tbody>
+            </table>
+            <div class="clearfix">
+                <ul class="paginationnIdeesProfs">
+                </ul>
+            </div>
+        </div>
+    </div>
+    <br>
+
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <h2>Boite à idées <b>Etudiants</b></h2>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Titre</th>
+                        <th>Sujet</th>
+                        <th>Voter oui</th>
+                        <th>Voter non</th>
+                        <th>% avis</th>
+                    </tr>
+                </thead>
+                <tbody id="listeIdeesEtudiants">
+
+                </tbody>
+            </table>
+            <div class="clearfix">
+                <ul class="paginationnIdeesEtudiants">
+                </ul>
+            </div>
+        </div>
+    </div>
+    <br>
+
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <h2>Proposer une <b>Idée</b></h2>
+                    </div>
+                </div>
+            </div>
+            <input class="col-md-5" type="text" id="newIdeeStudent" placeholder="Rentrez votre idée" />
+            <select id="sujetNewIdeeStudent">
+                <option value="Comptabilité">Comptabilité</option>
+                <option value="Marketing">Marketing</option>
+                <option value="Informatique">Informatique</option>
+                <option value="Electro-mécanique">Electro-mécanique</option>
+                <option value="Droit">Droit</option>
+            </select>
+            <button>Proposer mon idée</button>
+        </div>
+    </div>
+
+
+
 </main>
 
-<?php 
-    if(!isset($_SESSION['matricule'])){
-        echo("<script> connexion(); </script>");
-    }
+<?php
+if (!isset($_SESSION['matricule'])) {
+    echo ("<script> connexion(); </script>");
+}
 ?>
